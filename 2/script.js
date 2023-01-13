f6326fa6c5fc8ae556be19a7d9c5b5fbfe4d8878
Arnold KIENOU <arnoldkienou@gmail.com>
2023-01-13 21:57:17 +0000

    Fifth Release

--- 2/script.js
+++ 2/script.js
@@ -1,1 +1,47 @@
-//how to create carousel js 
+const wrap = document.querySelector(".wrap");
+const form = document.querySelector(".form");
+const container = document.querySelector(".container");
+const input = document.querySelector(".form-control");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const btn = document.querySelector(".btn");
+
+input.addEventListener('keyup', function(e){
+    const usernameText = username.value;
+    const passwordText = password.value;
+
+    if(usernameText != '' && passwordText != ''){
+        btn.attributes.removeNamedItem('disabled');
+    }else{
+        btn.attributes.setNamedItem('disabled');
+    }
+});
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const usernameText = username.value;
+    const passwordText = password.value;
+
+    if(usernameText != '' && passwordText != ''){
+        wrap.classList.add("down");
+        container.classList.add("fade-out");
+    }else{
+        btn.attributes.setNamedItem('disabled');
+    }
+});
+
+btn.addEventListener('mouseover', function(e){
+    const usernameText = username.value;
+    const passwordText = password.value;
+
+    if(usernameText == '' || passwordText == ''){
+        if(btn.parentElement.classList.contains('justify-end')){
+            btn.parentElement.classList.remove('justify-end');
+            btn.parentElement.classList.add('justify-start');
+        }
+        else if(btn.parentElement.classList.contains('justify-start')){
+            btn.parentElement.classList.remove('justify-start');
+            btn.parentElement.classList.add('justify-end');
+        }
+    }
+});
